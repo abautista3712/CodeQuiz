a14ee904b5e4f6b4f5a52c9eedd147b3e3db1220
Abelard Bautista <abelardbautista@gmail.com>
2020-01-24 22:13:48 -0800

    Created countdown timer in console.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 var startQuiz = document.querySelector("#startBtn");
 startQuiz.addEventListener("click", function() {
-  console.log("Test");
+  var countStart = 5;
+  setInterval(function() {
+    if (countStart === 0) {
+      return;
+    }
+    countStart--;
+    console.log(countStart);
+  }, 1000);
 });

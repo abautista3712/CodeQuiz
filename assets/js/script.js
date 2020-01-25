08de9c2f8e4489623f2837d93de1935b8e096813
Abelard Bautista <abelardbautista@gmail.com>
2020-01-25 00:01:30 -0800

    Deleted extra space in variable DominicanRepublic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,45 @@
 var startQuiz = document.querySelector("#startBtn");
 var targetTimeDiv = document.querySelector("#timeDiv");
 var Australia = {
-    "True": "Canberra",
-    "False":["Bridgetown","Saint John's","Kingston"]}
+  True: "Canberra",
+  False: ["Bridgetown", "Saint John's", "Kingston"]
+};
 var Australia = {
-    "True": "Phnom Penh",
-    "False":["Kinshasa","Moroni","Tbilisi"]}
+  True: "Phnom Penh",
+  False: ["Kinshasa", "Moroni", "Tbilisi"]
+};
 var Philippines = {
-    "True": "Manila",
-    "False":["Suva","Maseru","Nay Pyi Taw"]}
+  True: "Manila",
+  False: ["Suva", "Maseru", "Nay Pyi Taw"]
+};
 var Austria = {
-    "True": "Vienna",
-    "False":["Windhoek","Amsterdam","Oslo"]}
+  True: "Vienna",
+  False: ["Windhoek", "Amsterdam", "Oslo"]
+};
 var Columbia = {
-    "True": "Bogota",
-    "False":["Lima","San Marino","San Jose"]}
+  True: "Bogota",
+  False: ["Lima", "San Marino", "San Jose"]
+};
 var Australia = {
-    "True": "Sarajevo",
-    "False":["Yamoussoukro","Sofia","Minsk"]}
+  True: "Sarajevo",
+  False: ["Yamoussoukro", "Sofia", "Minsk"]
+};
 var Djibouti = {
-    "True": "Djibouti",
-    "False":["Gaborone","Porto-Novo","Algiers"]}
+  True: "Djibouti",
+  False: ["Gaborone", "Porto-Novo", "Algiers"]
+};
 var Belarus = {
-    "True": "Minsk",
-    "False":["Belmopan","Zagreb","Roseau"]}
-var Dominican Republic = {
-    "True": "Santo Domingo",
-    "False":["Buenos Aires","Quito","San Salvador"]}
+  True: "Minsk",
+  False: ["Belmopan", "Zagreb", "Roseau"]
+};
+var DominicanRepublic = {
+  True: "Santo Domingo",
+  False: ["Buenos Aires", "Quito", "San Salvador"]
+};
 var Canada = {
-    "True": "Ottawa",
-    "False":["Georgetown","Monrovia","Port Moresby"]}
+  True: "Ottawa",
+  False: ["Georgetown", "Monrovia", "Port Moresby"]
+};
 
 startQuiz.addEventListener("click", function() {
   var countStart = 5;

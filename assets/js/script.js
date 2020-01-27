5863c4914925e2fda29f356927e145ca1d40fe3d
Abelard Bautista <abelardbautista@gmail.com>
2020-01-26 18:00:15 -0800

    Added question and answers for first country.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,59 +4,71 @@ var targetTitle = document.querySelector("#title");
 var targetIntro = document.querySelector("#intro");
 var targetStartBtn = document.querySelector("#startBtn");
 var targetContentParent = document.querySelector("#contentParent");
+var questionCounter = 0;
 
-var countryArr = [
-  "Australia",
-  "Cambodia",
-  "Philippines",
-  "Austria",
-  "Colombia",
-  "Bosnia and Herzegovina",
-  "Djibouti",
-  "Belarus",
-  "Dominican Republic",
-  "Canada"
-];
 var Australia = {
+  Name: "Australia",
   True: "Canberra",
   False: ["Bridgetown", "Saint John's", "Kingston"]
 };
 var Cambodia = {
+  Name: "Cambodia",
   True: "Phnom Penh",
   False: ["Kinshasa", "Moroni", "Tbilisi"]
 };
 var Philippines = {
+  Name: "Philippines",
   True: "Manila",
   False: ["Suva", "Maseru", "Nay Pyi Taw"]
 };
 var Austria = {
+  Name: "Austria",
   True: "Vienna",
   False: ["Windhoek", "Amsterdam", "Oslo"]
 };
-var Columbia = {
+var Colombia = {
+  Name: "Colombia",
   True: "Bogota",
   False: ["Lima", "San Marino", "San Jose"]
 };
 var BosniaAndHerzegovina = {
+  Name: "Bosnia and Herzegovina",
   True: "Sarajevo",
   False: ["Yamoussoukro", "Sofia", "Minsk"]
 };
 var Djibouti = {
+  Name: "Djibouti",
   True: "Djibouti",
   False: ["Gaborone", "Porto-Novo", "Algiers"]
 };
 var Belarus = {
+  Name: "Belarus",
   True: "Minsk",
   False: ["Belmopan", "Zagreb", "Roseau"]
 };
 var DominicanRepublic = {
+  Name: "Dominican Republic",
   True: "Santo Domingo",
   False: ["Buenos Aires", "Quito", "San Salvador"]
 };
 var Canada = {
+  Name: "Canada",
   True: "Ottawa",
   False: ["Georgetown", "Monrovia", "Port Moresby"]
 };
+
+var countryArr = [
+  Australia,
+  Cambodia,
+  Philippines,
+  Austria,
+  Colombia,
+  BosniaAndHerzegovina,
+  Djibouti,
+  Belarus,
+  DominicanRepublic,
+  Canada
+];
 
 targetStartBtn.addEventListener("click", function() {
   // Start Timer
@@ -79,13 +91,22 @@ targetStartBtn.addEventListener("click", function() {
 
   //   New Question
   var newQuestion = document.createElement("h2");
-  newQuestion.textContent = "Test";
+  newQuestion.textContent =
+    "What is the capital of " + countryArr[questionCounter].Name + "?";
   targetContentParent.appendChild(newQuestion);
 
-  //   New
-  for (i = 0; i < 4; i++) {
+  //   New Answers
+  var trueAnswer = document.createElement("div");
+  trueAnswer.textContent = countryArr[questionCounter].True;
+  trueAnswer.style.padding = "1%";
+  trueAnswer.style.border = "solid";
+  targetContentParent.appendChild(trueAnswer);
+
+  for (i = 0; i < 3; i++) {
     var newAnswer = document.createElement("div");
-    newAnswer.textContent = "Test";
+    newAnswer.textContent = countryArr[questionCounter].False[i];
+    newAnswer.style.padding = "1%";
+    newAnswer.style.border = "solid";
     targetContentParent.appendChild(newAnswer);
   }
 });
